7decb8294bd5c7ec8b3d8223807dc08a5fa04fd7
Roshan Mallick <roshanmallick2025@gmail.com>
2026-07-29 01:30:28 +0530

    Refactor main function structure in NOT_gate.c

--- NOT_gate.c
+++ NOT_gate.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
     int A;
 
     printf("Enter 0 or 1 for NOT Gate: ");
     scanf("%d", &A);
 
-    if (A == 0 || A == 1)
-    {
+    if (A == 0 || A == 1){
         printf("Complement of A is %d\n", 1 - A);
-    }
-    else
-    {
+    }else{
         printf("Enter only 0 or 1.\n");
     }
 
